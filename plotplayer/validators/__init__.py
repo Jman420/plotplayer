e671f5a8f637b7c5234f7d8fd6fc739c5e871630
Justin Giannone <jgiannone@mdsol.com>
2018-02-27 17:08:48 -0500

    Add package & subpackage docstrings

---
+++ plotplayer/validators/__init__.py
@@ -0,0 +1,6 @@
+"""
+PlotPlayer Validators Subpackage contains various modules to support input and type validations.
+
+Public Modules:
+  * type_validation - Contains methods to validating various types
+"""
